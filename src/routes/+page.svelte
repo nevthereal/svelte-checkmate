83ba2fc3fa77939fe234932755d74db9c98bce85
Neville Brem <contact@nevillebrem.com>
2025-07-22 17:11:26 +0200

    tested some stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Login</h1>
+<script lang="ts">
+	import { login } from './auth.remote.js';
+</script>
+
+<main class="mx-auto max-w-3xl pt-8">
+	<h1 class="mb-4 text-2xl font-bold">Login Form example</h1>
+	<form {...login}>
+		<input class="rounded-lg border border-gray-300 p-2" type="text" name="email" />
+		<input class="rounded-lg border border-gray-300 p-2" type="password" name="password" />
+		<button class="rounded-lg bg-black p-2 text-white" type="submit">Login</button>
+	</form>
+</main>
--- src/routes/auth.remote.ts
+++ src/routes/auth.remote.ts
@@ -1,13 +1,27 @@
 import { form } from '$app/server';
 import { validator } from '$lib/validator.js';
+import { type } from 'arktype';
 import z from 'zod';
 
+const zodSchema = z.object({
+	email: z.email(),
+	password: z.string().min(6)
+});
+
+const arkschema = type({
+	email: 'string.email',
+	password: 'string'
+});
+
 export const login = form(async (formData) => {
-	validator({
-		schema: z.object({
-			email: z.email(),
-			password: z.string().min(6)
-		}),
+	const checked = await validator({
+		schema: zodSchema,
 		formData
 	});
+
+	if (!checked.success) return { success: false, error: checked.errors };
+
+	console.log(checked.data);
+
+	return { success: true };
 });
